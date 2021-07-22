6aa62636ede50274625922399f38432d4bc3ca65
임하나 <hanadul1@douzone.com>
2021-07-22 10:00:31 +0900

    구조 바꾸는중

--- src/App.js
+++ src/App.js
@@ -2,31 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import First from './First';
-import Second from './Second';
-import Third from './Third';
+import Main from './component/ContactPointTemplate/main';
+import AddContactPoint from './component/ContactPointTemplate/MenuComponent/AddContactPoint/AddContactPoint';
+import GroupList from './component/ContactPointTemplate/MenuComponent/GroupList/GroupList';
+import Search from './component/ContactPointTemplate/MenuComponent/Search/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <ul>
-            <li><Link to='/First'>First</Link></li>
-            <li><Link to='/Second'>Second</Link></li>
-            <li><Link to='/Third'>Third</Link></li>
-          </ul>
-        </div>
-        <div>
-          <Switch>
-            <Route path='/First' component={ First } />
-            <Route path='/Second' component={ Second } />ya
-            <Route path='/Third' component={ Third } />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <ul>
+              <li><Link to='/'>Main</Link></li>
+              <li><Link to='/AddContactPoint'>AddContactPoint</Link></li>
+              <li><Link to='/GroupList'>GroupList</Link></li>
+              <li><Link to='/Search'>Search</Link></li>
+            </ul>
+          </div>
+          <hr />
+          <div>
+            <Switch>
+              <Route exact path='/' component={ Main } />
+              <Route path='/AddContactPoint' component={ AddContactPoint } />
+              <Route path='/GroupList' component={ GroupList } />
+              <Route path='/Search' component={ Search } />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
